57504717846020ad856e8d45e903f596fe60b5b2
shyam640 <shyamvashishtha640@gmail.com>
2022-06-22 18:40:23 +0530

    Getting Started With Project

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+// import { BrowserRouter } from "react-router-dom";
+import reportWebVitals from './reportWebVitals';
+
+ReactDOM.render(<App/>,document.querySelector('#root'));
+
+reportWebVitals();
